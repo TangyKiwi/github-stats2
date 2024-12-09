730a73b163fc337e2fa38548dcd2b9cffa88daa1
TangyKiwi <32970087+TangyKiwi@users.noreply.github.com>
2024-12-09 00:23:00 -0600

    Update generate_images.py

--- generate_images.py
+++ generate_images.py
@@ -32,7 +32,7 @@ async def generate_overview(s: Stats) -> None:
     Generate an SVG badge with summary statistics
     :param s: Represents user's GitHub statistics
     """
-    with open("templates/overview.svg", "r") as f:
+    with open("templates/custom_overview.svg", "r") as f:
         output = f.read()
 
     output = re.sub("{{ name }}", await s.name, output)
@@ -54,7 +54,7 @@ async def generate_languages(s: Stats) -> None:
     Generate an SVG badge with summary languages used
     :param s: Represents user's GitHub statistics
     """
-    with open("templates/languages.svg", "r") as f:
+    with open("templates/custom_languages.svg", "r") as f:
         output = f.read()
 
     progress = ""
